53325b2a0d1f129fca218d0f04fdd45d523f0ffd
Ethan66 <2064281269@qq.com>
2017-11-01 09:47:22 +0800

    修改样式

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,20 @@
 <template>
   <div id="app">
-    <Nav1 class="nav1" v-bind:currentTab="currentTab" v-bind:icons='icons' />
-    <Editor class="editor" v-bind:currentTab="currentTab" v-bind:icons='icons' v-bind:resume="resume" />
+
   </div>
 </template>
 
 <script>
-  import Nav1 from './components/Nav1'
-  import Editor from './components/Editor'
+
 
 export default {
   name: 'app',
   components: {
-    Nav1,Editor
+
   },
   data(){
       return {
-          currentTab:{
-              tab:0
-          },
-        icons:['user','gongwenbao','xueshimao','skill','xiangmu']
-        ,
-        resume:{
-          profile:{
-              name:'',workDirection:'',email:'',number:'',age:'',website:'',imageUrl:'',address:'',summary:''
-          },
-          company:[
-            {name:'',department:'',position:'',date:'',content:''}
-          ],
-          education:[
-            {name:'',degree:'',profession:'',date:'',award:''}
-          ],
-          skills:[
-            {name:'',scope:'',grade:''}
-          ],
-          projects:[
-            {website:'',module:'',position:'',date:'',summary:''}
-          ]
-        }
+
       }
   },
   methods:{
@@ -60,11 +37,6 @@ export default {
     fill: currentColor;
     overflow: hidden;
   }
-  .nav1{
-
-  }
-  .editor{
-    flex: 1; background: #f2f2f2; margin-left: 24em;
-  }
+  
 
 </style>
